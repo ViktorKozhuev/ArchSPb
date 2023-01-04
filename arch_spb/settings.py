3c6c2962aee41ddef83f42d2eb922804b9d3f4ad
ViktorKozhuev <viktor.kozhuev@gmail.com>
2023-01-04 11:35:05 +0300

    Update settings

--- arch_spb/settings.py
+++ arch_spb/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-xh%$1m443=^gh#ojxok76_y7@$k!!hy8r%-1!)2r2-t$nx2=51'
+
 
 SECRET_KEY = DJANGO_SECRET_KEY
 
